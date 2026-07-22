646b0865c97c462977a47876d57e7ccc95bdabd4
girishmandal22556-stack <girishmandal22556@gmail.com>
2026-07-22 15:14:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let audio = document.getElementById("music");
 
+function play() {
+    audio.play();
+}
+
+function pause() {
+    audio.pause();
+}
+
+function volume(value) {
+    audio.volume = value;
+}
+
+// DJ style effects
+function increaseBass() {
+    alert("Bass effect activated 🎚️");
+}
+
+function nextSong() {
+    alert("Next song loading 🎵");
+}
